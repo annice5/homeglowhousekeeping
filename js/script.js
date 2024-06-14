b48b73a4e7c9342d3bef04f80707dcaeca78ae9d
Agnes odame <agnesodame5@gmail.com>
2024-06-14 13:07:53 +0000

    add changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 
 
-const btn1= document.getElementById('btn1')
-btn1.onclick = function(){
+const btn1 = document.getElementById('btn1')
+btn1.onclick = function () {
     alert('Get Pricing & Book In 60 Seconds')
 }
 
-const btn2= document.getElementById('btn2')
-btn2.onclick = function(){
+const btn2 = document.getElementById('btn2')
+btn2.onclick = function () {
     alert('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corporis doloribus consequatur fugit voluptatum ex rerum perspiciatis cupiditate, esse hic!')
 }
